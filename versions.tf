419c7552d822c39e70bdef9ab198f521fceeb06b
Tim Campbell <timetinytim@gmail.com>
2024-06-26 14:53:45 +0200

    Update tf and module verions

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.8.0"
 
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
-      version = "~> 1.31.0"
+      version = "~> 1.47.0"
     }
   }
 }
